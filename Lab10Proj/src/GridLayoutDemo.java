3ef2e0b5df2bae3d9ac6f945c6c4b350f1a82753
cti22d109 <cristian_burca29@yahoo.com>
2023-12-18 18:34:31 +0200

    Lab_2023-12-18

--- Lab10Proj/src/GridLayoutDemo.java
+++ Lab10Proj/src/GridLayoutDemo.java
@@ -12,48 +12,69 @@ import java.awt.event.WindowEvent;
 public class GridLayoutDemo extends JFrame {
 
     GridLayoutDemo() {
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent windowEvent){
-                Toolkit.getDefaultToolkit().beep();
-                JOptionPane.showMessageDialog(null, "Va rog sa folositi butonul \"EXIT\" pentru a iesi");
-                setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-                //System.exit(0);
-            }
-        });
         setResizable(false);
         //================================================
         JPanel p1 = new JPanel();
         p1.setLayout(new GridLayout(7, 2));
+
         FlowLayout layout = new FlowLayout();
         JPanel p2 = new JPanel();
         p2.setLayout(layout);
         JLabel one, two, three, four;
-        JTextField tname, tsalary, tcode, tdesig;
+        JTextField fNume, fCnp, fFunctie, fSalar;
         JButton buttonSave, buttonExit;
-        JCheckBox cs1, cs2, cs3;
-        one = new JLabel("NAME");
-        tname = new JTextField(20);
-        two = new JLabel("CODE");
-        tcode = new JTextField(20);
-        three = new JLabel("DESIGNATION");
-        tdesig = new JTextField(20);
-        four = new JLabel("SALARY");
-        tsalary = new JTextField(20);
-        buttonSave = new JButton("SAVE");
-        buttonExit = new JButton("EXIT");
-        cs1 = new JCheckBox("Casuta 1");
-        cs2 = new JCheckBox("Casuta 2");
-        cs3 = new JCheckBox("Casuta 3");
+        JRadioButton cs1, cs2;
+        one = new JLabel("Nume:");
+        fNume = new JTextField(20);
+        two = new JLabel("CNP:");
+        fCnp = new JTextField(20);
+        three = new JLabel("Functia:");
+        fFunctie = new JTextField(20);
+        four = new JLabel("Salar:");
+        fSalar = new JTextField(20);
+        buttonSave = new JButton("Salvare");
+        buttonExit = new JButton("Anulare");
+        cs1 = new JRadioButton("Barbat");
+        cs2 = new JRadioButton("Femeie");
         p1.add(one);
-        p1.add(tname);
+        p1.add(fNume);
         p1.add(two);
-        p1.add(tcode);
+        p1.add(fCnp);
         p1.add(three);
-        p1.add(tdesig);
+        p1.add(fFunctie);
         p1.add(four);
-        p1.add(tsalary);
+        p1.add(fSalar);
         p1.add(cs1);
         p1.add(cs2);
+        cs1.setSelected(true);
+        cs1.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    if(cs1.isSelected()) {
+                        cs2.setSelected(false);
+                    }
+                    if(!cs1.isSelected()) {
+                        cs1.setSelected(true);
+                    }
+        }
+        });
+        cs2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if(cs2.isSelected()) {
+                    cs1.setSelected(false);
+                }
+                if(!cs2.isSelected()) {
+                    cs2.setSelected(true);
+                }
+            }
+        });
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent){
+                Toolkit.getDefaultToolkit().beep();
+                JOptionPane.showMessageDialog(null, "Va rog sa folositi butonul \"" + buttonExit.getText() + "\" pentru a iesi");
+                setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+                //System.exit(0);
+            }
+        });
         //=========================================================
         p2.add(buttonSave);
         p2.add(buttonExit);
@@ -62,6 +83,7 @@ public class GridLayoutDemo extends JFrame {
         setVisible(true);
         this.setSize(500, 400);
         buttonExit.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent e) {
                 if (buttonExit.isEnabled()) {
                     System.exit(0);
@@ -75,4 +97,5 @@ public class GridLayoutDemo extends JFrame {
     {
         new GridLayoutDemo();
     }
+
 } 
